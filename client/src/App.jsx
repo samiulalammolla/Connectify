ce3561d77cc6acd1048772e69962cb7f87cebc41
RajeshVerma1612 <verma.rajesh123v@gmail.com>
2024-02-24 19:14:14 +0530

    Create Navigation Bar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,13 @@ import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
+import NavBar from "./components/NavBar";
 
 function App() {
 
   return (
     <BrowserRouter>
+      <NavBar/>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/sign-in" element={<SignIn/>}/>
---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+function NavBar() {
+  return (
+    <header className='bg-slate-200 shadow-md'>
+      <div className='flex justify-between items-center p-4 max-w-6xl mx-auto'>
+        <Link to='/'>
+        <h1 className='font-bold text-md sm:text-xl italic'>Connetify</h1>
+        </Link>
+        <form className='bg-slate-100  rounded-md '>
+          <input type="text" placeholder='Search...' className='bg-transparent focus:outline-none w-32 sm:w-64 p-1 ' />
+        </form>
+        <ul className='flex gap-4 font-semibold text-slate-700 justify-center items-center'>
+          <Link to='/'>
+          <li className='hidden sm:inline hover:underline'>Home</li>
+          </Link>
+          <Link to='/profile'>
+          <li className='hidden sm:inline hover:underline'>Profile</li>
+          </Link>
+          <Link to='/sign-in'>
+          <li className='text-yellow-50 sm:inline hover:bg-blue-400 bg-blue-500 py-1 px-2 rounded-md '>Sign in</li>
+          </Link>
+        </ul>
+      </div>
+    </header>
+  )
+}
+
+export default NavBar

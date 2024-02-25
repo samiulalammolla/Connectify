854152ffe3681eb19ba5c2105f3662553de22754
RajeshVerma1612 <verma.rajesh123v@gmail.com>
2024-02-26 00:16:04 +0530

    Connect to Database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';//we cannot use .env variabe inside a package so install dotenv library and .env file is ignored while pushing on github
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('Connected to MongoDb');
+}).catch((err)=>{
+    console.log(err);
+})
+
 
 const app = express();
 
